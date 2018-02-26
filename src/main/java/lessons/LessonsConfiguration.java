536ced1b10ab479115b9562b299dcc830086d42a
* H I R U R G * <hirurg_03@ukr.net>
2018-02-26 18:08:04 +0200

    Initial commit

---
+++ src/main/java/lessons/LessonsConfiguration.java
@@ -0,0 +1,15 @@
+package lessons;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lessons.services.GreetingService;
+import lessons.services.GreetingServiceImpl;
+
+@Configuration
+public class LessonsConfiguration {
+	@Bean
+	GreetingService greetingService() {
+		return new GreetingServiceImpl();
+	}
+}
